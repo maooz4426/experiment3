2b1e0174d1850f654489c6c84c567fc39ad6a95d
maooz4426 <maooz4426@gmail.com>
2024-05-29 19:23:14 +0900

    titleの位置を修正

--- Assets/TitleViewManager.cs
+++ Assets/TitleViewManager.cs
@@ -6,7 +6,9 @@ public class TitleViewManager : MonoBehaviour
 {
     private void Start()
     {
-        PlayerController playerController = GameObject.FindWithTag(("Player")).GetComponent<PlayerController>();
+        GameObject player = GameObject.FindWithTag(("Player"));
+        player.transform.position = new Vector3(0, 0, -15);
+        PlayerController playerController = player.GetComponent<PlayerController>();
         
         playerController.TitleStart();
     }
